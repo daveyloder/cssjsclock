3435ee99d1399cd828a2186850d4aca94618471b
David Loder <davidloder92@gmail.com>
2021-01-09 15:29:00 -0500

    convert 24 hour time to 12 hour time

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const displayTime = document.querySelector('.display-time');
+
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds/60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes/60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
     const hours = now.getHours();
-    const hoursDegrees = ((hours/60) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
-    const time = hours + ":" + minutes + ":" + seconds
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+function formatTime(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    let hours = now.getHours();
+    let midday = "AM"
+    if (hours > 12) {
+     hours = now.getHours()- 12;
+     midday = "PM"
+    } 
+    const time = hours + ":" + minutes + ":" + seconds + " " + midday;
+    displayTime.innerHTML = `<h2>${time}</h2>`
     console.log(time);
 }
+setInterval(formatTime, 1000);
 setInterval(setDate, 1000);
